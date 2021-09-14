d6c3765a55b906995cf45806aabb978d3388ef84
Nuno Barros <nunobarros@devgo.org>
2021-09-14 15:39:15 -0100

    rework and modify the home page

--- DocumentManagement/src/DocumentManagement.Web/Pages/Index.cshtml.cs
+++ DocumentManagement/src/DocumentManagement.Web/Pages/Index.cshtml.cs
@@ -1,25 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using DocumentManagement.Core.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Open.Linq.AsyncExtensions;
 
 namespace DocumentManagement.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly IDocumentTypeStore _documentTypeStore;
+        private readonly IDocumentService _documentService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IDocumentTypeStore documentTypeStore, IDocumentService documentService)
         {
-            _logger = logger;
+            _documentTypeStore = documentTypeStore;
+            _documentService = documentService;
         }
 
-        public void OnGet()
-        {
+        [BindProperty] public string DocumentTypeId { get; set; } = default!;
+        [BindProperty] public IFormFile FileUpload { get; set; } = default!;
 
+        public ICollection<SelectListItem> DocumentTypes { get; set; } = new List<SelectListItem>();
+
+        public async Task OnGetAsync(CancellationToken cancellationToken)
+        {
+            var documentTypes = await _documentTypeStore.ListAsync(cancellationToken).ToList();
+            DocumentTypes = documentTypes.Select(x => new SelectListItem(x.Name, x.Id)).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync(CancellationToken cancellationToken)
+        {
+            var extension = Path.GetExtension(FileUpload.FileName);
+            var fileName = $"{Guid.NewGuid()}{extension}";
+            var fileStream = FileUpload.OpenReadStream();
+            var document = await _documentService.SaveDocumentAsync(fileName, fileStream, DocumentTypeId, cancellationToken);
+
+            return RedirectToPage("FileReceived", new {DocumentId = document.Id});
         }
     }
 }
