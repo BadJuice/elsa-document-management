6fc5fbd2642094ded5ff88dba1c7c68a6f343b70
Nuno Barros <nunobarros@devgo.org>
2021-09-15 12:11:53 -0100

    add some annotations in the store implementation file

--- DocumentManagement/src/DocumentManagement.Persistence/Services/EFCoreDocumentStore.cs
+++ DocumentManagement/src/DocumentManagement.Persistence/Services/EFCoreDocumentStore.cs
@@ -14,7 +14,16 @@ namespace DocumentManagement.Persistence.Services
         {
             _dbContextFactory = dbContextFactory;
         }
-        
+
+        // One improvement one might consider is making the SaveAsync method of the EFCoreDocumentStore
+        // class thread-safe by doing an upsert operation server-side rather than client side. Right now,
+        // if two threads were to try and save a document object with the same ID at the same time,
+        // chances are high that you end up with two records in the database.
+
+        /*
+         * To do a server-side upsert operation, you might consider using a 3rd party package such as
+         * EFCore.BulkExtensions, FlexLabs.Upsert or Entity Framework Extensions.
+        */
         public async Task SaveAsync(Document entity, CancellationToken cancellationToken = default)
         {
             await using var dbContext = _dbContextFactory.CreateDbContext();
